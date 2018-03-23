3179510605c755a41d6f107f6976aff926e0073b
ccboys <walter@intrising.com.tw>
2018-03-23 17:59:05 +0800

    [second modify] use jq to add active class

--- assets/js/function/module.js
+++ assets/js/function/module.js
@@ -24,8 +24,8 @@ $("#module-video-large").load("module/video-large.html");
 
 //////// Sub menu ////////
 $("#module-menu-sub").load("module/module-menu-sub.html", function () {
-  console.log('done loading submenu');
   $('#module-menu-sub .submenu .nav-item').on('click', function(){
+    $('#module-menu-sub .submenu .nav-item a').removeClass('active')
     console.log('click the sub');
     // window.localStorage.removeItem('activeItem');
     var mapObject = {
@@ -45,18 +45,17 @@ $("#module-menu-sub").load("module/module-menu-sub.html", function () {
 
     var targetType = $(this).parents('.submenu').data('type');
 
-    console.log('activeItem is', activeItem);
-    console.log('currentMenuIndex is', currentMenuIndex);
-    console.log('currentMenuText is', currentMenuText);
-    console.log('targetType is', targetType);
+    // console.log('activeItem is', activeItem);
+    // console.log('currentMenuIndex is', currentMenuIndex);
+    // console.log('currentMenuText is', currentMenuText);
+    // console.log('targetType is', targetType);
 
     if (currentMenuIndex === 0 || currentMenuIndex === 1 || currentMenuIndex === 4){
       window.location.href = mapPathObject[targetType] + activeItem;
     }
 
     if (currentMenuIndex === 2 && targetType === 'type01') {
-      console.log('window.location.href is', window.location.href)
-      if (window.location.href.indexOf('event-cloud-1.html') == -1) {
+      if (window.location.href.indexOf('event-cloud-1.html') === -1) {
         window.location.href = "event-cloud-1.html" + activeItem
       }
       return;
@@ -69,6 +68,7 @@ $("#module-menu-sub").load("module/module-menu-sub.html", function () {
     } else if(currentMenuIndex === 3 && targetType === 'type01'){
       window.location.href = mapPathObject[targetType] + activeItem;
     }
+
   });
   $('#module-menu-sub .submenu').mouseleave(function () {
     $(this).removeClass('slideDown');
